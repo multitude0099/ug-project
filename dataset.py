d87f295032dfc5bb8a22ea4b0235df4d2640b329
ankur <s.ankursonawane@gmail.com>
2019-10-22 01:16:28 +0530

    Relocate Data to separate directory

--- dataset.py
+++ dataset.py
@@ -9,7 +9,7 @@ end = '18:00:00'
 
 
 def suny_international():
-    df = pd.read_csv('full_data.csv', skiprows=2, parse_dates=[['Year', 'Month', 'Day', 'Hour', 'Minute']],
+    df = pd.read_csv('data/full_data.csv', skiprows=2, parse_dates=[['Year', 'Month', 'Day', 'Hour', 'Minute']],
                      index_col=0, date_parser=lambda x: datetime.strptime(x, '%Y %m %d %H %M'))
     df.index.name = 'date'
     df = df.drop(
