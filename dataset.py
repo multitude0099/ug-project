bf9e2857778f586fae2a2c9dde8474b81b3e99a8
ankur <s.ankursonawane@gmail.com>
2019-10-23 04:38:13 +0530

    Added mew dataswe

--- dataset.py
+++ dataset.py
@@ -22,3 +22,31 @@ def suny_international():
     df = df.between_time(start, end)
     # df.columns = ['dew', 'temp', 'press', 'rel_humid', 'zenith_ang', 'wnd_dir', 'wnd_spd', 'ghi']
     return df
+
+
+class hi_seas:
+    """
+    These datasets are meteorological data from the HI-SEAS weather station from four months (September through December 2016) between Mission IV and Mission V.
+
+    For each dataset, the fields are:
+
+    A row number (1-n) useful in sorting this export's results The UNIX time_t date (seconds since Jan 1, 1970). Useful in sorting this export's results with other export's results The date in yyyy-mm-dd format The local time of day in hh:mm:ss 24-hour format The numeric data, if any (may be an empty string) The text data, if any (may be an empty string)
+
+    The units of each dataset are:
+
+    Solar radiation: watts per meter^2
+
+    Temperature: degrees Fahrenheit
+
+    Humidity: percent
+    Barometric pressure: Hg
+    Wind direction: degrees
+    Wind speed: miles per hour
+    Sunrise/sunset: Hawaii time
+
+    """
+
+
+    @classmethod
+    def load_data():
+        pass
